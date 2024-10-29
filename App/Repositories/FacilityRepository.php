0e27fee94f8fe57442f6f7f3c605c6a1295bdfa4
superzaky <superkytoz@gmail.com>
2024-10-29 23:13:34 +0100

    Transactions

--- App/Repositories/FacilityRepository.php
+++ App/Repositories/FacilityRepository.php
@@ -2,11 +2,12 @@
 
 namespace App\Repositories;
 
-use App\Plugins\Http\Exceptions;
+use App\Plugins\Http\Exceptions\InternalServerError;
 use App\Models\Facility;
 use App\Plugins\Db\Db;
 
 use App\Plugins\Di\Factory;
+use Exception;
 
 class FacilityRepository {
     /** @var Db|null */
@@ -21,66 +22,19 @@ class FacilityRepository {
     @throws Exception if an operation fails
     */
     public function create(Facility $facility): \stdClass | bool {
-        $query = 'INSERT INTO facilities (name, creation_date, location_id) VALUES (:name, :creation_date, :location_id)';
-        $bind = [':name' => $facility->get_name(), ':creation_date' =>  date("Y-m-d"), 
-            ':location_id' => $facility->get_location_id()];
-
-        if ($this->db->executeQuery($query, $bind)) {
-            $query = 'SELECT *
-                        FROM facilities
-                        WHERE id = :id';
-            $facility_id = $this->db->getLastInsertedId();
-            $bind = [':id' => $facility_id];
-
-            foreach ($facility->get_tags() as $tag) {
-                $insert_tag_query = 'INSERT IGNORE INTO tags (name) VALUES (:name)';
-                $bind_tag_query = [':name' => $tag->get_name()];
-                $this->db->executeQuery($insert_tag_query, $bind_tag_query);
-                $select_tag_query = 'SELECT *
-                    FROM tags
-                    WHERE name = :name';
-
-                $tag_id = $this->db->getLastInsertedId();
-                $existing_tag = $this->db->fetchObject($select_tag_query, $bind_tag_query);
-               
-                if ($tag_id == 0) {
-                    $tag_id = $existing_tag->id;
-                } 
-                
-                $insert_facility_tags_query = 'INSERT INTO facility_tags (facility_id, tag_id) VALUES (:facility_id, :tag_id)';
-                $bind_facility_tags_query = [':facility_id' => $facility_id, 'tag_id' => $tag_id];
-
-                $this->db->executeQuery($insert_facility_tags_query, $bind_facility_tags_query);
-            }
-
-            $fetched_object = $this->db->fetchObject($query, $bind);
-
-            return $this->retrieve($fetched_object->id);
-        } else {
-            return null;
-        }
-    }
-
-    public function retrieve(int $id): \stdClass | bool {
-        $query = 'SELECT locations.*, facilities.*, GROUP_CONCAT(tags.name) as tags
-            FROM facilities
-            JOIN locations ON facilities.location_id = locations.id
-            LEFT JOIN facility_tags ON facilities.id = facility_tags.facility_id
-            LEFT JOIN tags ON facility_tags.tag_id = tags.id
-            WHERE facilities.id = :id';
-        $bind = [':id' => $id];
-        return $this->db->fetchObject($query, $bind);
-    }
-
-    public function update(Facility $facility): \stdClass | bool {
-        $query = 'UPDATE facilities SET name=:name, location_id=:location_id WHERE id = :id';
-        $bind = [':name' => $facility->get_name(), ':location_id' => $facility->get_location_id(),
-            ':id' => $facility->get_id()];
-
-        if ($this->db->executeQuery($query, $bind)) {
-            $query = 'DELETE FROM facility_tags WHERE facility_id = :id';
-            $bind = [':id' => $facility->get_id()];
+        $this->db->beginTransaction();
+        try {
+            $query = 'INSERT INTO facilities (name, creation_date, location_id) VALUES (:name, :creation_date, :location_id)';
+            $bind = [':name' => $facility->get_name(), ':creation_date' =>  date("Y-m-d"), 
+                ':location_id' => $facility->get_location_id()];
+    
             if ($this->db->executeQuery($query, $bind)) {
+                $query = 'SELECT *
+                            FROM facilities
+                            WHERE id = :id';
+                $facility_id = $this->db->getLastInsertedId();
+                $bind = [':id' => $facility_id];
+    
                 foreach ($facility->get_tags() as $tag) {
                     $insert_tag_query = 'INSERT IGNORE INTO tags (name) VALUES (:name)';
                     $bind_tag_query = [':name' => $tag->get_name()];
@@ -97,28 +51,98 @@ class FacilityRepository {
                     } 
                     
                     $insert_facility_tags_query = 'INSERT INTO facility_tags (facility_id, tag_id) VALUES (:facility_id, :tag_id)';
-                    $bind_facility_tags_query = [':facility_id' => $facility->get_id(), 'tag_id' => $tag_id];
+                    $bind_facility_tags_query = [':facility_id' => $facility_id, 'tag_id' => $tag_id];
     
                     $this->db->executeQuery($insert_facility_tags_query, $bind_facility_tags_query);
                 }
-                $query = 'SELECT * FROM facilities WHERE id = :id';
-                $bind = [':id' => $facility->get_id()];
-                $fetched_object = $this->db->fetchObject($query, $bind);
     
-                return $this->retrieve($fetched_object->id);
+                $fetched_object = $this->db->fetchObject($query, $bind);
+                $fetched_object = $this->retrieve($fetched_object->id);
+                $this->db->commit();
+                return $fetched_object;
             } else {
                 return null;
             }
-
-        } else {
-            return null;
+        } catch (Exception $e) {
+            $this->db->rollBack();
+            throw new InternalServerError($e);
         }
     }
 
-    public function delete(int $id): \stdClass | bool {
-        $query = 'DELETE FROM facilities WHERE id = :id';
+    public function retrieve(int $id): \stdClass | bool {
+        $query = 'SELECT locations.*, facilities.*, GROUP_CONCAT(tags.name) as tags
+            FROM facilities
+            JOIN locations ON facilities.location_id = locations.id
+            LEFT JOIN facility_tags ON facilities.id = facility_tags.facility_id
+            LEFT JOIN tags ON facility_tags.tag_id = tags.id
+            WHERE facilities.id = :id';
         $bind = [':id' => $id];
-        return $this->db->executeQuery($query, $bind);
+        return $this->db->fetchObject($query, $bind);
+    }
+
+    public function update(Facility $facility): \stdClass | bool {
+        $this->db->beginTransaction();
+        try {
+            $query = 'UPDATE facilities SET name=:name, location_id=:location_id WHERE id = :id';
+            $bind = [':name' => $facility->get_name(), ':location_id' => $facility->get_location_id(),
+                ':id' => $facility->get_id()];
+    
+            if ($this->db->executeQuery($query, $bind)) {
+                $query = 'DELETE FROM facility_tags WHERE facility_id = :id';
+                $bind = [':id' => $facility->get_id()];
+                if ($this->db->executeQuery($query, $bind)) {
+                    foreach ($facility->get_tags() as $tag) {
+                        $insert_tag_query = 'INSERT IGNORE INTO tags (name) VALUES (:name)';
+                        $bind_tag_query = [':name' => $tag->get_name()];
+                        $this->db->executeQuery($insert_tag_query, $bind_tag_query);
+                        $select_tag_query = 'SELECT *
+                            FROM tags
+                            WHERE name = :name';
+        
+                        $tag_id = $this->db->getLastInsertedId();
+                        $existing_tag = $this->db->fetchObject($select_tag_query, $bind_tag_query);
+                       
+                        if ($tag_id == 0) {
+                            $tag_id = $existing_tag->id;
+                        } 
+                        
+                        $insert_facility_tags_query = 'INSERT INTO facility_tags (facility_id, tag_id) VALUES (:facility_id, :tag_id)';
+                        $bind_facility_tags_query = [':facility_id' => $facility->get_id(), 'tag_id' => $tag_id];
+        
+                        $this->db->executeQuery($insert_facility_tags_query, $bind_facility_tags_query);
+                    }
+                    $query = 'SELECT * FROM facilities WHERE id = :id';
+                    $bind = [':id' => $facility->get_id()];
+
+                    $fetched_object = $this->db->fetchObject($query, $bind);
+                    $fetched_object = $this->retrieve($fetched_object->id);
+                    $this->db->commit();
+                    return $fetched_object;
+                } else {
+                    return null;
+                }
+    
+            } else {
+                return null;
+            }
+        } catch (Exception $e) {
+            $this->db->rollBack();
+            throw new InternalServerError($e);
+        }
+    }
+
+    public function delete(int $id): bool {
+        $this->db->beginTransaction();
+        try {
+            $query = 'DELETE FROM facilities WHERE id = :id';
+            $bind = [':id' => $id];
+            $is_deleted = $this->db->executeQuery($query, $bind);
+            $this->db->commit();
+            return $is_deleted;
+        } catch (Exception $e) {
+            $this->db->rollBack();
+            throw new InternalServerError($e);
+        }
     }
 
     public function searchBy(string $facility_name = null, string $tag_name = null, string $city = null): array | bool {
